0ebaedb42d240ab9c0d10cc0357feef23813dcb9
Max Kless <maxk@nrwl.io>
2022-10-05 20:59:49 +0200

    some more aliases

--- oh-my-zsh/aliases-git.zsh
+++ oh-my-zsh/aliases-git.zsh
@@ -2,3 +2,4 @@
 alias gcsm='git commit -S -m'
 alias gpnew='git push -u origin HEAD'
 alias gcnv='git commit --no-verify -m'
+alias gro1='git rebase --onto origin/master HEAD~1'
--- oh-my-zsh/aliases-nx.zsh
+++ oh-my-zsh/aliases-nx.zsh
@@ -1,3 +1,5 @@
 function nxra {
     npx nx@latest run-many --target=$1 --parallel --all
 }
+
+alias ydocs='yarn documentation'
